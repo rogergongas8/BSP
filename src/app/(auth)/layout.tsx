d1ca208ffb3e05fdcc4747366b37ddcf9f621a48
Roger Gonzalbo <roger.gonzalbo8@gmail.com>
2026-08-16 15:40:18 +0200

    test

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -40,14 +40,14 @@ const CATS: Record<string, {
     width: 637,
     height: 1004,
     alt: '',
-    style: { width: '90%', maxWidth: 410, bottom: 200, right: -140 },
+    style: { width: '90%', maxWidth: 410, bottom: 50, right: -120 },
   },
   '/signup': {
     src: '/images/login/registercat.png',
     width: 446,
     height: 1030,
     alt: '',
-    style: { width: '50%', maxWidth: 195, bottom: 100, right: -44 },
+    style: { width: '50%', maxWidth: 195, bottom: 50, right: -44 },
   },
 }
 
